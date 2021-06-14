933e2332e028cfce5d82c85e577e431428677050
Amit Elyasi <elyasiamit@gmail.com>
2021-06-14 16:04:24 +0300

    DONE

--- hw2/kmeans_pp.py
+++ hw2/kmeans_pp.py
@@ -1,19 +1,34 @@
+"""
+Kmeans_pp implementation in Python
+Software Project ex. 2
+
+Amit Elyasi
+316291434
+
+Nizan Shemi
+206962912
+"""
+
 import mykmeanssp as kmeans
 import numpy as np
 import pandas as pd
 import sys
 
+
+"""
+implementation of kmeans++, finding a good initialization of centroids for the "kmeans" algorithm
+"""
 def Kmeans_pp(datapoints,k):
     n = len(datapoints)
     datapoints = np.array([np.array(point) for point in datapoints])
     centroids = [0]
     centroids[0] = np.random.choice(n,1)[0]
-    dists=np.full((n,k),sys.maxsize)
+    dists = np.zeros((n,k))
     min_dists = np.zeros(n)
     for z in range(k-1):
         for i in range(n):
             dists[i][z] = np.inner(datapoints[i]-datapoints[centroids[z]], datapoints[i]-datapoints[centroids[z]])
-            min_dists[i] = min(dists[i])
+            min_dists[i] = min(dists[i][0:z+1])
         dists_sum = np.sum(min_dists)
         probabilities = [dist/dists_sum for dist in min_dists]
 
@@ -22,6 +37,11 @@ def Kmeans_pp(datapoints,k):
     return centroids
 
 
+"""
+given 2 file names, where the files containes data points with the first column being the index, 
+reads the files and joins them with inner join (key=the first column) 
+returns pandas DataFrame containing the joint data 
+"""
 def readAndJoin(file1, file2):
     data1 = pd.read_csv(file1, header=None)
     data2 = pd.read_csv(file2, header=None)
@@ -32,6 +52,9 @@ def readAndJoin(file1, file2):
     return data
 
 
+"""
+given an array of arrays, returns a 1D python array with all of the entrys of the matrix in one long vector
+"""
 def arrToSeq(arr):
     return [item for vec in arr for item in vec]
 
@@ -45,20 +68,13 @@ def seqRoundAndPrint(seq, d):
         print(seq[i * d + d-1])
 
 
-
-
-# def main():
-#     k = 3
-#     datapoints_table = readAndJoin('test_data\input_1_db_1.txt', 'test_data\input_1_db_2.txt')
-#     datapoints_matrix = datapoints_table.values.tolist()
-#     centroidArray = Kmeans_pp(datapoints_matrix, k)
-#     print(centroidArray)
-
-
 def main():
     np.random.seed(0)
     args = sys.argv
-    if len(args) >= 5:
+    if len(args)<4 or len(args)>5:
+        print("INPUT ERROR:\nthe number of argument is incorrect")
+        return 1
+    if len(args) == 5:
         k, max_iter, file1, file2 = args[1], args[2], args[3], args[4]
     else:
         k, file1, file2 = args[1], args[2], args[3]
