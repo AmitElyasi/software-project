ee26576bd5e0fdabf48d6a91d71ae30c3f7c174c
nizanshami-tau <73163063+nizanshami-tau@users.noreply.github.com>
2021-09-03 13:36:47 +0300

    Update spkmeans.h

--- project/spkmeans.h
+++ project/spkmeans.h
@@ -2,12 +2,16 @@
 
 
 
-/*start of functions for Normalized Spectral Clustering implementation*/
+/*find the eigangap heuristic and return the number of cluster*/
+int calc_eiganvalue_gap(float *mat, float *sorted_eiganvalues, int n);
 
 /* given A real symmetric matrix updates A and a matrix V that A is diagonal and all of A eigenvalues
 on the diagonal and V columns is the correspondence eigenvector
 */
 void jacobi_algorithm_for_eigenvalues(float *A, float *V, int n);
+
+/*initial V to the uint matrix*/
+void form_V(float *mat, int n);
 
 void form_diagonal_mat(float *diagonal_mat, float *weighted_adj_mat , int n);
 
@@ -21,8 +25,6 @@ void calc_normalized_laplacian(float *normalized_laplacian, float *diagonal_mat,
  * the indexes of the off-diagonal largest absolute value */
 void indexes_of_max_off_diag(float *mat, int *row, int *col, int dim);
 
-/*find the eigangap heuristic and return the number of cluster*/
-int calc_eiganvalue_gap(float *mat, float *sorted_eiganvalues, int n);
 
 /** given a square matrix (mat) ,
  * the indexes of its off-diagonal largest absolute value (i,j)
@@ -35,6 +37,26 @@ float get_t(float *mat, int i, int j, int dim);
  */
 float get_c(float t);
 
+/* given matrix mat calculates the off(mat)^2 = notm_f(mat) - sum_(i=1)^n a_ii*/
+float calc_off_square(float *mat,int n);
+
+/**
+ * given diagonal matrix [n*n] and an empty array [n], fills the array with the diagonal values
+ */
+void diag_to_array(float *diag, float *arr, int n);
+
+/** given array of length dim, and its dimention, fills the array with integers
+ * such that arr[i]=i
+ */
+void fill_ordered_ints(int *arr, int dim);
+
+/**
+ * given an array- arr, and another array- indexes, both of length dim,
+ * fills indexes with integers [0,dim-1] such that their order dictates a sorted order on arr values.
+ * (using QuickSort algorithm)
+ */
+void quickSort_indexes(float *arr, int *indexes, int dim);
+
 /** fill given matrix with the data from input file */
 void read_data(FILE* fp, float *data_points, char *line, int n, int dim);
 
@@ -43,9 +65,37 @@ void read_data(FILE* fp, float *data_points, char *line, int n, int dim);
 */
 float distance(float *v1, float *v2, int dim,int row_v1, int row_v2);
 
+/** given the eigenvectors matrix [n*n] (as columns), an array [n] that dictates an order on the eigenvalues,
+ * n (the mat dimention), k, and a result matrix [n*k],
+ * fills the result matrix with the normalized first k eigenvectors as columns
+ */
+void normalized_k_eigenvectors(float *eigenvecs, int *indexes, int n, int k, float *result);
+
+/**
+ * returns arr[i][j]
+ */
 float get(float* arr, int i, int j, int dim);
 
+/**
+ * Sets arr[j][i]=item
+ */
 void set(float* arr, int i, int j, int dim, float item);
+
+/*return target matrix for goal == "wam" */
+float *wam(float *data_points, int n, int dim);
+
+/*return an array that contains the diagnoal of the target mateix for goal == "ddg" */
+float *ddg(float *data_points,int n, int dim);
+
+/*return target matrix for goal == "lnorm" */
+float *lnorm(float *data_points, int n, int dim);
+
+/*return target matrix in shape (n+1,n) the first row is the eigonvalue the other n row is the eigonvector matrix when the goal == "lnorm" */
+float *jacobi(float *data_points, int n);
+
+/*return target matrix T in shape (n,k) when goal == "spk"*/
+float *spk(float *data_points, int n , int dim, int *k);
+
 
 static int kmeans(int k, float* data_points, float* centroids, float* utl ,int max_iter, int dim, int n){
 
